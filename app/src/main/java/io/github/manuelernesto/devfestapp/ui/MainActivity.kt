f03638883f7c372111ae4269bb6ed18ade47ebd4
manuelernesto <manuelernesto@outlook.com>
2019-08-21 14:47:28 +0100

    setup app

---
+++ app/src/main/java/io/github/manuelernesto/devfestapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package io.github.manuelernesto.devfestapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.manuelernesto.devfestapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
